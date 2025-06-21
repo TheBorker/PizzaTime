b1b2ca3b036cbb1c380c05ef96f905714f8d5af6
TheBorker <rkindinger1@gmail.com>
2025-06-21 16:05:40 -0400

    Added Patch file, check desc
    Next step: add the required patches for item and scrap spawning and interactions!

---
+++ PillarJohnscript/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+
+namespace PillarJohnscript
+{
+    [BepInPlugin("com.pillarjohn.pillarjohncore", "PillarJohnCore", "1.0.0")]
+    public class PillarJohnCore : BaseUnityPlugin 
+    {
+        private readonly Harmony harmony = new Harmony("com.pillarjohn.pillarjohncore");
+        private static PillarJohnCore instance;
+        internal ManualLogSource nls;
+        void Awake()
+        {
+            instance = this;
+            
+            Logger.LogInfo("PillarJohnCore plugin loaded successfully.");
+            // Initialize the logger
+            nls = BepInEx.Logging.Logger.CreateLogSource("PillarJohnCore");
+            nls.LogInfo("Logger initialized successfully, Pillar John is watching...");
+            // Apply Harmony patches
+            harmony.PatchAll(typeof(PillarJohnCore));
+        }
+        
+    }
+}
